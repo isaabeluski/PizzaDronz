65cebcae02545d39d2864e2d134dcafc20bb73da
isaabeluski <s2096890@ed.ac.uk>
2022-09-27 13:58:52 +0100

    test

---
+++ src/main/java/uk/ac/ed/inf/LngLat.java
@@ -0,0 +1,13 @@
+package uk.ac.ed.inf;
+
+public class LngLat {
+
+    public double lng;
+    public double lat;
+
+    public LngLat(double lng, double lat) {
+        this.lng = lng;
+        this.lat = lat;
+    }
+
+}
